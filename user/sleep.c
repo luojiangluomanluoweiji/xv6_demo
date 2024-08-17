3633636d29815bc5e21a3d9f6c717ffbbb66e7fd
luojiang <ybh3163003038@gmail.com>
2024-08-17 21:35:22 +0800

    sleep_1

---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc,char* argv[])
+{
+  if(argc < 2 )
+  {
+    printf("Usage:%s<num> \n",argv[0]);
+    exit(1);
+  }
+  if(sleep(atoi(argv[1]))<=0)
+  {
+    printf("sleep error");
+  }
+  exit(0);
+
+}
